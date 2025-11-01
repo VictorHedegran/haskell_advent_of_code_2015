ba42b11a845dc24356ab8e5f263053372287964d
victor-desktop <victor.hedegran@gmail.com>
2025-11-01 14:56:02 +0100

    add solution to day 1 part 2

---
+++ day_1/part_2/app/Main.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldl" #-}
+{-# HLINT ignore "Use foldr" #-}
+module Main where
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (i:is) = i + sumList is
+
+mapCharToNumber :: Char -> Int
+mapCharToNumber '(' = 1
+mapCharToNumber ')' = -1
+mapCharToNumber _ = 0
+    
+findBasementIndex :: [Int] -> Int -> Int -> Int
+findBasementIndex [] _ _ = -1
+findBasementIndex (i:is) index floorsum 
+    | floorsum + i == -1 = index + 1 
+    | otherwise = findBasementIndex is (index + 1) (floorsum + i)
+
+main :: IO ()
+main = do
+    contents <- getContents
+    print (findBasementIndex (map mapCharToNumber contents) 0 0 )
